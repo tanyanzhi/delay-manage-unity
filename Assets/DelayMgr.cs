d721864a2710b8bead24413cd29f7ef3b40ccb49
tanyanzhi <345723624@qq.com>
2015-07-29 20:50:06 +0800

    aa

---
+++ Assets/DelayMgr.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//delay manager
+public class DelayMgr : MonoBehaviour{
+
+    public static DelayMgr Instance = null;
+
+    void Awake() {
+
+        Instance = this;
+
+    }
+
+    //delay callback with no args
+    public void delay(float delayTime, Callback callback){
+
+        StartCoroutine(startDelay(delayTime, callback));
+
+    }
+
+    //delay callback with 1 args
+    public void delay<T>(float delayTime, T args1, Callback<T> callback) {
+
+        StartCoroutine(startDelay<T>(delayTime, args1, callback));
+
+    }
+
+    //delay callback with 2 args
+    public void delay<T, U>(float delayTime, T args1, U args2, Callback<T, U> callback) {
+
+        StartCoroutine(startDelay<T, U>(delayTime, args1, args2, callback));
+
+    }
+
+    //delay callback with 3 args
+    public void delay<T, U, V>(float delayTime, T args1, U args2, V args3, Callback<T, U, V> callback) {
+
+        StartCoroutine(startDelay<T, U, V>(delayTime, args1, args2, args3, callback));
+
+    }
+
+    //delay callback with 4 args
+    public void delay<T, U, V, W>(float delayTime, T args1, U args2, V args3, W args4, Callback<T, U, V, W> callback) {
+
+        StartCoroutine(startDelay<T, U, V, W>(delayTime, args1, args2, args3, args4, callback));
+
+    }
+
+
+
+    private IEnumerator startDelay(float delayTime, Callback callback) {
+
+        yield return new WaitForSeconds(delayTime);
+
+        callback();
+
+    }
+
+    private IEnumerator startDelay<T>(float delayTime, 
+                                      T args1, 
+                                      Callback<T> callback) {
+
+        yield return new WaitForSeconds(delayTime);
+
+        callback(args1);
+
+    }
+
+    private IEnumerator startDelay<T, U>(float delayTime, 
+                                         T args1, 
+                                         U args2, 
+                                         Callback<T, U> callback) {
+
+        yield return new WaitForSeconds(delayTime);
+
+        callback(args1, args2);
+
+    }
+
+    private IEnumerator startDelay<T, U, V>(float delayTime, 
+                                            T args1, 
+                                            U args2, 
+                                            V args3, 
+                                            Callback<T, U, V> callback) {
+
+        yield return new WaitForSeconds(delayTime);
+
+        callback(args1, args2, args3);
+
+    }
+
+    private IEnumerator startDelay<T, U, V, W>(float delayTime, 
+                                               T args1, 
+                                               U args2, 
+                                               V args3, 
+                                               W args4, 
+                                               Callback<T, U, V, W> callback) {
+
+        yield return new WaitForSeconds(delayTime);
+
+        callback(args1, args2, args3, args4);
+
+
+    }
+	
+}
+
+public delegate void Callback();
+public delegate void Callback<T>(T args1);
+public delegate void Callback<T, U>(T args1, U args2);
+public delegate void Callback<T, U, V>(T args1, U args2, V args3);
+public delegate void Callback<T, U, V, W>(T args1, U args2, V args3, W args4);
---
+++ Assets/Example.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //delay 1 second : no args
+        DelayMgr.Instance.delay(1, () => {
+
+            Debug.Log("delay 1 second : no args");
+
+        });
+
+        //delay 2 second : 1 args
+        DelayMgr.Instance.delay<string>(2, "args1", (string args1) => {
+
+            Debug.Log("delay 2 second : " + args1);
+
+        });
+
+        //delay 3 second : 2 args
+        DelayMgr.Instance.delay<string, string>(3, "args1", "args2", (string args1, string args2) => {
+
+            Debug.Log("delay 3 second : " + args1 + " " + args2);
+
+        });
+
+        //delay 4 second : 3 args
+        DelayMgr.Instance.delay<string, string, string>(4, "args1", "args2", "args3", (string args1, string args2, string args3) => {
+
+            Debug.Log("delay 4 second : " + args1 + " " + args2 + " " + args3);
+
+        });
+
+        //delay 5 second : 4 args
+        DelayMgr.Instance.delay<string, string, string, string>(5, "args1", "args2", "args3", "args4", (string args1, string args2, string args3, string args4) => {
+
+            Debug.Log("delay 5 second : " + args1 + " " + args2 + " " + args3 + " " + args4);
+
+        });
+	
+	}
+	
+}
